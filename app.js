0dfb12be211c5b9ffc86e54b8ea00fb9cedca4d8
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-09-30 18:37:19 -0400

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './5616.jpg',
+    image2: './56160.jpg',
+    speedIn: 2,
+    speedOut: 2,
+    displacementImage: './diss.png'
+})
